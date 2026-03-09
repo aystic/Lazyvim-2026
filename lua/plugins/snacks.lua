0fd4df9358d8e6ff14d882cf7065cd6110e039e0
Mohit Pathak <itspmohit@gmail.com>
2026-03-09 21:01:13 +0530

    feat: configure snacks explorer

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,32 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              width = 30,
+              backdrop = false,
+              -- min_width = 40,
+              -- height = 0,
+              -- position = "left",
+              -- border = "none",
+              -- box = "vertical",
+              -- {
+              --   win = "input",
+              --   height = 1,
+              --   border = true,
+              --   title = "{title} {live} {flags}",
+              --   title_pos = "center",
+              -- },
+              -- { win = "list", border = "none" },
+              -- { win = "preview", title = "{preview}", height = 0.4, border = "top" },
+              -- position = "right",
+            },
+          },
+        },
+      },
+    },
+  },
+}
