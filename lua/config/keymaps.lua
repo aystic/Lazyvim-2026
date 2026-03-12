4b3d9d0a4e9f6567e52373c5e80bdd7c9001f26f
Mohit Pathak <itspmohit@gmail.com>
2026-03-12 20:53:09 +0530

    feat: add options and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,51 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local keymap = vim.keymap
+local opts = { silent = true, noremap = true }
+
+-- Do not yank with x & p
+keymap.set("n", "x", '"_x')
+keymap.set("v", "p", '"_dP', opts)
+
+-- Delete a word backwords without it overwriting the clipboard
+keymap.set("n", "dw", 'vb"_d')
+
+-- Increment/Decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Select all
+keymap.set("n", "<C-a>", "ggVG")
+
+-- keymap.set for moving buffer left right using bufferline
+keymap.set("n", "<C-n>", ":BufferLineMovePrev<CR>", opts)
+keymap.set("n", "<C-m>", ":BufferLineMoveNext<CR>", opts)
+
+-- Write a file
+keymap.set("n", "<leader>cw", ":w<CR>", { silent = true, noremap = true, desc = "Save" })
+
+keymap.del("n", "<leader>l")
+
+keymap.set("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
+keymap.set("n", "<leader>lw", "<cmd>w<cr><esc>", { desc = "Save File" })
+keymap.set("n", "<leader>lf", function()
+  LazyVim.format({ force = true })
+end, { desc = "Format" })
+
+-- Visual --
+-- Stay in indent mode
+keymap.set("v", "<", "<gv^", opts)
+keymap.set("v", ">", ">gv^", opts)
+
+-- Move text up and down
+keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap.set("x", "J", ":m '>+1<CR>gv=gv", opts)
+keymap.set("x", "K", ":m '<-2<CR>gv=gv", opts)
+keymap.set("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap.set("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,37 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.snacks_animate = false
+
+-- LazyVim completion engine to use.
+-- Can be one of: nvim-cmp, blink.cmp
+-- Leave it to "auto" to automatically use the completion engine
+-- enabled with `:LazyExtras`
+vim.g.lazyvim_cmp = "auto"
+
+-- Show the current document symbols location from Trouble in lualine
+-- You can disable this for a buffer by setting `vim.b.trouble_lualine = false`
+vim.g.trouble_lualine = true
+
+local opt = vim.opt
+
+opt.expandtab = true -- Use spaces instead of tabs
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.shiftwidth = 2 -- Size of an indent
+opt.textwidth = 100
+opt.wrap = false -- Disable line wrap
+opt.linebreak = true -- Wrap lines at convenient points
+opt.mouse = "nvch" -- Enable mouse mode
+opt.number = true -- Print line number
+opt.relativenumber = true -- Relative line numbers
+opt.scrolloff = 10 -- Lines of context
+opt.sidescrolloff = 10 -- Columns of context
+opt.smoothscroll = false
+opt.termguicolors = true -- True color support
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
+
 vim.g.python3_host_prog = "/Users/pmohit/.pyenv/versions/neovim/bin/python3"
 vim.g.loaded_perl_provider = 0
